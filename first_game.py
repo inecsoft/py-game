d1e98e4e7867e77ef80f2f0c738fcf9a97b09897
inecsoft <ivanpedrouk@gmail.com>
2020-06-04 17:04:01 +0100

    first game ran

---
+++ first_game.py
@@ -0,0 +1,27 @@
+import pygame
+# Import and initialize the pygame library
+pygame.init()
+
+#set up the drawing window
+screen = pygame.display.set_mode((500,500))
+
+#running untill the user exits
+running = True
+
+while running:
+    # if the user clicked on the close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    # fill screen with white background
+    screen.fill((255,255,255))
+    
+    # Draw a solid blue circle in the centre
+    pygame.draw.circle(screen,(0,0,255),(250,250), 30)
+    
+    # Flip the display
+    pygame.display.flip()
+
+#Done! time to quit
+pygame.quit()
